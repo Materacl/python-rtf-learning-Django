93ea3ac77ddae5bce10bfa48408cc913b9019128
Materacl <yurchenko.stas15@gmail.com>
2024-01-21 02:00:32 +0500

    добавлен код для создания графиков и графики на странице

---
+++ vacancies_analysis/demand_graphs.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def make_salary_plot(salary_data: pd.DataFrame, name: str, name_plot: str):
+    plt.figure(figsize=(10, 6))
+    plt.plot(salary_data['year'], salary_data['average_salary'], marker='o', label='Average Salary')
+    plt.title(name_plot)
+    plt.xlabel('Год')
+    plt.ylabel('Средняя зарплата')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig('data/' + name)
+    plt.show()
+
+
+def make_count_plot(vacancy_data: pd.DataFrame, name: str, name_plot: str):
+    plt.figure(figsize=(10, 6))
+    plt.plot(vacancy_data['year'], vacancy_data['vacancy_count'], marker='o', color='orange', label='Vacancy Count')
+    plt.title(name_plot)
+    plt.xlabel('Год')
+    plt.ylabel('Количество вакансий')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig('data/' + name)
+    plt.show()
+
+
+def main():
+    dynamic_count_all = pd.read_csv('data/dynamic_count_all.csv')
+    dynamic_count_prof = pd.read_csv('data/dynamic_count_prof.csv')
+    dynamic_salary_all = pd.read_csv('data/dynamic_salary_all.csv')
+    dynamic_salary_prof = pd.read_csv('data/dynamic_salary_prof.csv')
+
+    make_salary_plot(dynamic_salary_all, name="dynamic_salary_all_plot",
+                     name_plot="Динамика уровня зарплат по годам")
+    make_salary_plot(dynamic_salary_prof, name="dynamic_salary_prof_plot",
+                     name_plot="Динамика уровня зарплат по годам для С++ Разработчика")
+    make_count_plot(dynamic_count_all, name="dynamic_count_all_plot",
+                    name_plot="Динамика количества вакансий по годам")
+    make_count_plot(dynamic_count_prof, name="dynamic_count_prof_plot",
+                    name_plot="Динамика количества вакансий по годам для С++ Разработчика")
+
+
+if __name__ == "__main__":
+    main()
