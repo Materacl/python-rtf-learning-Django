c0c481854383c0b0aeb0381e90b0481bff03e63f
Materacl <yurchenko.stas15@gmail.com>
2024-01-21 23:44:02 +0500

    Переделана страница с навыками, чтобы можно было выбрать год

--- profession_project/skills/views.py
+++ profession_project/skills/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from .models import TopSkillsAll, TopSkillsProf
 
-
 def skills_page(request):
     unique_years = list(range(2017, 2024))
+    selected_year = request.GET.get('year', unique_years[-1])  # По умолчанию выбран последний год
 
-    skills_all = {year: list(TopSkillsAll.objects.filter(year=year)) for year in unique_years}
-    skills_prof = {year: list(TopSkillsProf.objects.filter(year=year)) for year in unique_years}
+    skills_all = TopSkillsAll.objects.filter(year=selected_year)
+    skills_prof = TopSkillsProf.objects.filter(year=selected_year)
 
     context = {
         'unique_years': unique_years,
+        'selected_year': int(selected_year),
         'skills_all': skills_all,
         'skills_prof': skills_prof,
         'prof': 'C++ Разработчик',
